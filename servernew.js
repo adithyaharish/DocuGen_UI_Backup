e2f21d644cf6f7adaa68f62a63eb67fbcc4f8248
Adithya <adithyaharish10gmail.com>
2025-04-01 10:23:29 -0400

    servernew file for deploy

--- servernew.js
+++ servernew.js
@@ -201,3 +201,11 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
