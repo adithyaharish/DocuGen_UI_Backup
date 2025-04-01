350260903b4d48b03c675cf1de89a939c5743d89
Adithya <adithyaharish10gmail.com>
2025-04-01 11:22:55 -0400

    localhost fix

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
 
     try {
-      const response = await fetch("http://localhost:5000/generate-docs", {
+      const response = await fetch("/generate-docs", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -97,7 +97,7 @@ function App() {
 
 
     try {
-      const response = await fetch("http://localhost:5000/chat", {
+      const response = await fetch("/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
